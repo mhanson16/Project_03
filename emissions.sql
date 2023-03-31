bc2af1fe2a4d63c780ed2720aa1b840688e53ac0
Xiaolin Ruan <kelly@Xiaolins-MacBook-Pro.local>
2023-03-30 21:30:01 -0700

    sql

---
+++ emissions.sql
@@ -0,0 +1,62 @@
+-- Drop Tables if Existing
+DROP TABLE IF EXISTS emissions_by_countries cascade;
+
+-- Create table and view column datatypes
+CREATE TABLE emissions_by_countries (
+    country VARCHAR(100) ,
+    iso VARCHAR(100) ,
+    year INT ,
+    total FLOAT ,
+	coal FLOAT ,
+	oil FLOAT ,
+	gas FLOAT ,
+	cement FLOAT ,
+	flaring FLOAT ,
+	other FLOAT 
+);
+
+-- View the table to ensure all data has been imported correctly
+SELECT * FROM emissions_by_countries;
+
+-- Clean the table 
+-- 1. Delete rows where year less than 1945
+DELETE FROM emissions_by_countries
+WHERE year < 1945;
+
+-- 2. Fill out the missing ISO code
+UPDATE emissions_by_countries
+SET iso = 'FEA'
+WHERE Country = 'French Equatorial Africa';
+
+UPDATE emissions_by_countries
+SET iso = 'FWA'
+WHERE Country = 'French West Africa';
+
+UPDATE emissions_by_countries
+SET iso = 'KOF'
+WHERE Country = 'Kuwaiti Oil Fires';
+
+UPDATE emissions_by_countries
+SET iso = 'LEI'
+WHERE Country = 'Leeward Islands';
+
+UPDATE emissions_by_countries
+SET iso = 'PIP'
+WHERE Country = 'Pacific Islands (Palau)';
+
+UPDATE emissions_by_countries
+SET iso = 'RYI'
+WHERE Country = 'Ryukyu Islands';
+
+-- 3. Rename the countries with symbol
+UPDATE emissions_by_countries
+SET country = 'Cote dIvoire'
+WHERE iso = 'CIV';
+
+UPDATE emissions_by_countries
+SET country = 'Curacao'
+WHERE iso = 'CUW';
+
+UPDATE emissions_by_countries
+SET country = 'Reunion'
+WHERE iso = 'REU';
