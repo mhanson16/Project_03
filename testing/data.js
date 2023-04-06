27fe212b603c3b79559020aeb46932834caa5dae
ltablas <luciatablas@gmail.com>
2023-04-05 19:39:12 -0700

    adding testing folder

---
+++ testing/data.js
@@ -0,0 +1,92 @@
+let top2021Emissions = [{
+    'Qatar': {
+        'Total': 35.587357,
+        'Coal': 0.0,
+        'Oil': 3.226295,
+        'Gas': 31.072901,
+        'Cement': 0.58465,
+        'Flaring': 0.7035119999999999,
+        'Other': 0.0
+    },
+    'Bahrain': {
+        'Total': 26.66372,
+        'Coal': 0.0,
+        'Oil': 2.426313,
+        'Gas': 23.642933,
+        'Cement': 0.42008799999999996,
+        'Flaring': 0.17438599999999999,
+        'Other': 0.0
+    },
+    'Kuwait': {
+        'Total': 24.972098,
+        'Coal': 0.0,
+        'Oil': 12.643539,
+        'Gas': 11.721402,
+        'Cement': 0.287619,
+        'Flaring': 0.319538,
+        'Other': 0.0
+    },
+    'Trinidad and Tobago': {
+        'Total': 23.677599,
+        'Coal': 0.0,
+        'Oil': 2.016713,
+        'Gas': 21.305957,
+        'Cement': 0.172285,
+        'Flaring': 0.182644,
+        'Other': 0.0
+    },
+    'Brunei Darussalam': {
+        'Total': 23.532003,
+        'Coal': 0.865973,
+        'Oil': 8.708841,
+        'Gas': 13.046933,
+        'Cement': 0.0,
+        'Flaring': 0.9102549999999999,
+        'Other': 0.0
+    },
+    'United Arab Emirates': {
+        'Total': 21.792151,
+        'Coal': 0.714923,
+        'Oil': 6.389958,
+        'Gas': 13.90118,
+        'Cement': 0.611617,
+        'Flaring': 0.174473,
+        'Other': 0.0
+    },
+    'New Caledonia': {
+        'Total': 19.097542,
+        'Coal': 9.177665,
+        'Oil': 9.817779,
+        'Gas': 0.0,
+        'Cement': 0.102099,
+        'Flaring': 0.0,
+        'Other': 0.0
+    },
+    'Saudi Arabia': {
+        'Total': 18.702989,
+        'Coal': 0.0,
+        'Oil': 10.28421,
+        'Gas': 7.5121590000000005,
+        'Cement': 0.797994,
+        'Flaring': 0.108627,
+        'Other': 0.0
+    },
+    'Oman': {
+        'Total': 17.916536,
+        'Coal': 0.0,
+        'Oil': 2.697747,
+        'Gas': 13.787812,
+        'Cement': 0.401763,
+        'Flaring': 1.029214,
+        'Other': 0.0
+    },
+    'Australia': {
+        'Total': 15.091473,
+        'Coal': 5.823767,
+        'Oil': 5.368438,
+        'Gas': 2.979896,
+        'Cement': 0.10879799999999999,
+        'Flaring': 0.6663589999999999,
+        'Other': 0.14421499999999998
+    }
+}]
