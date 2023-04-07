69b133d4b73537a2990309808cd1e4eeb9543f2e
kellyruan42 <120171721+kellyruan42@users.noreply.github.com>
2023-04-06 21:04:57 -0700

    Add files via upload

--- emissions.sql
+++ emissions.sql
@@ -3,20 +3,22 @@ DROP TABLE IF EXISTS emissions_by_countries cascade;
 
 -- Create table and view column datatypes
 CREATE TABLE emissions_by_countries (
-    country VARCHAR(100) ,
-    iso VARCHAR(100) ,
-    year INT ,
-    total FLOAT ,
-	coal FLOAT ,
-	oil FLOAT ,
-	gas FLOAT ,
-	cement FLOAT ,
-	flaring FLOAT ,
-	other FLOAT 
+    country VARCHAR(100),
+    iso VARCHAR(100),
+    year INT,
+    total FLOAT,
+	coal FLOAT,
+	oil FLOAT,
+	gas FLOAT,
+	cement FLOAT,
+	flaring FLOAT,
+	other FLOAT
 );
 
 -- View the table to ensure all data has been imported correctly
-SELECT * FROM emissions_by_countries;
+SELECT * 
+FROM emissions_by_countries
+WHERE iso = 'AFG';
 
 -- Clean the table 
 -- 1. Delete rows where year less than 1945
@@ -60,3 +62,41 @@ WHERE iso = 'CUW';
 UPDATE emissions_by_countries
 SET country = 'Reunion'
 WHERE iso = 'REU';
+
+-- 4. Replace Nulls with 0s
+UPDATE emissions_by_countries
+SET total = 0
+WHERE total IS NULL;
+
+UPDATE emissions_by_countries
+SET coal = 0
+WHERE coal IS NULL;
+
+UPDATE emissions_by_countries
+SET oil = 0
+WHERE oil IS NULL;
+
+UPDATE emissions_by_countries
+SET gas = 0
+WHERE gas IS NULL;
+
+UPDATE emissions_by_countries
+SET cement = 0
+WHERE cement IS NULL;
+
+UPDATE emissions_by_countries
+SET flaring = 0
+WHERE flaring IS NULL;
+
+UPDATE emissions_by_countries
+SET other = 0
+WHERE other IS NULL;
+
+-- 5. Reorder the year column
+SELECT * FROM emissions_by_countries 
+ORDER BY country, year ASC;
+
+-- 6. Select the year from 2017 to 2021
+SELECT * FROM emissions_by_countries
+WHERE year >= 2017
+ORDER BY year, country ASC;
